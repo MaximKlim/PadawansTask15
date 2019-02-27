0b3258cbab86ee058c607f21dd92447bdef8aa83
Maksim Klimentsyeu <maksim_klimentsyeu@epam.com>
2019-02-27 22:42:42 +0300

    Update EumerableTask.cs

--- EumerableTask.cs
+++ EumerableTask.cs
@@ -6,108 +6,134 @@ namespace PadawansTask15
 {
     public class EnumerableTask
     {
-        /// <summary> Transforms all strings to upper case.</summary>
-        /// <param name="data">Source string sequence.</param>
-        /// <returns>
-        ///   Returns sequence of source strings in uppercase.
-        /// </returns>
-        /// <example>
-        ///    {"a", "b", "c"} => { "A", "B", "C" }
-        ///    { "A", "B", "C" } => { "A", "B", "C" }
-        ///    { "a", "A", "", null } => { "A", "A", "", null }
-        /// </example>
         public IEnumerable<string> GetUppercaseStrings(IEnumerable<string> data)
         {
-            // TODO : Implement GetUppercaseStrings
-            throw new NotImplementedException();
+            IEnumerator<string> ie = data.GetEnumerator();
+
+            List<string> result = new List<string>();
+
+            string newItem;
+
+            while (ie.MoveNext())
+            {
+                string item = ie.Current;
+                if (!string.IsNullOrEmpty(item))
+                {
+                    newItem = item.ToUpper();
+                }
+                else
+                {
+                    newItem = item;
+                }
+
+                result.Add(newItem);
+            }
+            return result;
         }
 
-        /// <summary> Transforms an each string from sequence to its length.</summary>
-        /// <param name="data">Source strings sequence.</param>
-        /// <returns>
-        ///   Returns sequence of strings length.
-        /// </returns>
-        /// <example>
-        ///   { } => { }
-        ///   {"a", "aa", "aaa" } => { 1, 2, 3 }
-        ///   {"aa", "bb", "cc", "", "  ", null } => { 2, 2, 2, 0, 2, 0 }
-        /// </example>
+
         public IEnumerable<int> GetStringsLength(IEnumerable<string> data)
         {
-            // TODO : Implement GetStringsLength
-            throw new NotImplementedException();
+            List<int> result = new List<int>();
+            int temp;
+
+            foreach (string e in data)
+            {
+                if (e == null)
+                {
+                    result.Add(0);
+                }
+                else
+                {
+                    result.Add(e.Length);
+                }
+
+            }
+            return result;
         }
 
-        /// <summary>Transforms integer sequence to its square sequence, f(x) = x * x. </summary>
-        /// <param name="data">Source int sequence.</param>
-        /// <returns>
-        ///   Returns sequence of squared items.
-        /// </returns>
-        /// <example>
-        ///   { } => { }
-        ///   { 1, 2, 3, 4, 5 } => { 1, 4, 9, 16, 25 }
-        ///   { -1, -2, -3, -4, -5 } => { 1, 4, 9, 16, 25 }
-        /// </example>
+
         public IEnumerable<long> GetSquareSequence(IEnumerable<int> data)
         {
-            // TODO : Implement GetSquareSequence
-            throw new NotImplementedException();
+            long temp;
+            List<long> result = new List<long>();
+
+            foreach (int e in data)
+            {
+                temp = e * e;
+                result.Add(temp);
+            }
+            return result;
         }
 
-        /// <summary> Filters a string sequence by a prefix value (case insensitive).</summary>
-        /// <param name="data">Source string sequence.</param>
-        /// <param name="prefix">Prefix value to filter.</param>
-        /// <returns>
-        ///  Returns items from data that started with required prefix (case insensitive).
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Thrown when prefix is null.</exception>
-        /// <example>
-        ///  { "aaa", "bbbb", "ccc", null }, prefix = "b"  =>  { "bbbb" }
-        ///  { "aaa", "bbbb", "ccc", null }, prefix = "B"  =>  { "bbbb" }
-        ///  { "a","b","c" }, prefix = "D"  => { }
-        ///  { "a","b","c" }, prefix = ""   => { "a","b","c" }
-        ///  { "a","b","c", null }, prefix = ""   => { "a","b","c" }
-        ///  { "a","b","c" }, prefix = null => ArgumentNullException
-        /// </example>
+
         public IEnumerable<string> GetPrefixItems(IEnumerable<string> data, string prefix)
         {
-            // TODO : Implement GetPrefixItems
-            throw new NotImplementedException();
+            List<string> result = new List<string>();
+
+            if (prefix == null)
+            {
+                throw new ArgumentNullException { };
+            }
+            string prefixLow = prefix.ToLower();
+            string prefixUp = prefix.ToUpper();
+
+
+
+            foreach (string e in data)
+            {
+                if (e == null)
+                {
+                }
+                else if ((e.StartsWith(prefixLow)) || (e.StartsWith(prefixUp)))
+                {
+                    result.Add(e);
+                }
+
+            }
+            return result;
         }
 
-        /// <summary> Finds the 3 largest numbers from a sequence.</summary>
-        /// <param name="data">Source sequence.</param>
-        /// <returns>
-        ///   Returns the 3 largest numbers from a sequence.
-        /// </returns>
-        /// <example>
-        ///   { } => { }
-        ///   { 1, 2 } => { 2, 1 }
-        ///   { 1, 2, 3 } => { 3, 2, 1 }
-        ///   { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } => { 10, 9, 8 }
-        ///   { 10, 10, 10, 10 } => { 10, 10, 10 }
-        /// </example>
+
         public IEnumerable<int> Get3LargestItems(IEnumerable<int> data)
         {
-            // TODO : Implement Get3LargestItems
-            throw new NotImplementedException();
+            List<int> list = new List<int>();
+            int a;
+            List<int> result = new List<int>();
+
+            list.AddRange(data);
+            list.Sort();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if (list.Count == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    a = list.Max();
+                    result.Add(a);
+                    list.Remove(a);
+                }
+
+            }
+
+            return result;
         }
 
-        /// <summary> Calculates sum of all integers from object array.</summary>
-        /// <param name="data">Source array.</param>
-        /// <returns>
-        ///    Returns the sum of all integers from object array.
-        /// </returns>
-        /// <example>
-        ///    { 1, true, "a", "b", false, 1 } => 2
-        ///    { true, false } => 0
-        ///    { 10, "ten", 10 } => 20 
-        ///    { } => 0
-        /// </example>
         public int GetSumOfAllIntegers(object[] data)
         {
-            // TODO : Implement GetSumOfAllIntegers
-            throw new NotImplementedException();
+
+            int sum = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] is int)
+                {
+                    sum = sum + (Convert.ToInt32(data[i]));
+                }
+            }
+            return sum;
         }
     }
 }
